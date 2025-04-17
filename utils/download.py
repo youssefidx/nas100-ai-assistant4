24ae681e881542611d431298fcad6d411ba0054b
youssefidx <131968165+youssefidx@users.noreply.github.com>
2025-04-17 14:17:05 +0000

    Add files via upload

---
+++ utils/download.py
@@ -0,0 +1,8 @@
+
+import base64
+
+def get_table_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="trade_log.csv">📥 Download Trade Log as CSV</a>'
+    return href
