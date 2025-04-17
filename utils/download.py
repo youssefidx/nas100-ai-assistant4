6d7685fe9c3aec874c0237646856bdea5bf46398
youssefidx <131968165+youssefidx@users.noreply.github.com>
2025-04-17 14:16:13 +0000

    Delete utils/download.py

--- utils/download.py
+++
@@ -1,8 +0,0 @@
-import base64
-import pandas as pd
-
-def get_table_download_link(df: pd.DataFrame, filename="trade_log.csv"):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">📥 Download Trade Log as CSV</a>'
-    return href
